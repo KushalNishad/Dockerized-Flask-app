10d1a89a81be1415d523bc0709faf0b84c240c03
KushalNishad <nkushal77@gmail.com>
2025-04-22 22:47:37 -0400

    Update dbconfig.py

--- database/db_config.py
+++ database/db_config.py
@@ -2,9 +2,9 @@ import pymysql
 
 def get_db_connection():
     connection = pymysql.connect(
-        host='mydb.cylck8yh5jkc.eu-central-1.rds.amazonaws.com',  # AWS RDS Endpoint
-        user='dbuser',        # Replace with your RDS username
-        password='dbpassword',  # Replace with your RDS password
+        host='mydb.c98a60agsfko.us-east-2.rds.amazonaws.com',  # AWS RDS Endpoint
+        user='flask',        # Replace with your RDS username
+        password='flask_pass',  # Replace with your RDS password
         db='devprojdb',       # Replace with your database name
         charset='utf8mb4',
         cursorclass=pymysql.cursors.DictCursor
